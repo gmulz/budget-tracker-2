3dac2983b304e9c1ca7faf7e1075dd119bede69d
Grant Mulitz <themulitzer@gmail.com>
2021-08-29 12:15:40 -0400

    Create transaction from json request

--- server/budgettrackerbackend/budgets/views.py
+++ server/budgettrackerbackend/budgets/views.py
@@ -14,12 +14,14 @@ def index(request):
 class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all().order_by('description')
     serializer_class = CategorySerializer
+    #create a category
+    #delete a category
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('name')
     serializer_class = UserSerializer
 
-#transactions per user
+
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
@@ -29,5 +31,19 @@ class TransactionViewSet(viewsets.ModelViewSet):
         request_user = request.query_params['user']
         transactions = Transaction.objects.filter(user=request_user).values()
         return Response(transactions)
+    
+    #todo create transaction by user, one at a time
+    @action(methods=['post'], detail=False)
+    def create_transaction(self, request, pk=None):
+        transaction_obj = request.data
+        new_transaction = Transaction(description=transaction_obj['description'], 
+                    date=transaction_obj['date'],
+                    cost=transaction_obj['cost'],
+                    category_id=transaction_obj['category'],
+                    user_id=transaction_obj['user'])
+        new_transaction.save()
+        return Response({'status': 'created transaction'})
+    #edit transaction by id
+    #delete a transaction
     
 #income per user
