c19092451db4bd123e5bb97479035b6fb2d5a369
Grant Mulitz <themulitzer@gmail.com>
2021-09-03 09:39:30 -0400

    Adding recurring sidebar styles

--- budget-tracker/src/main-area/sidebar/RecurringBlotters.tsx
+++ budget-tracker/src/main-area/sidebar/RecurringBlotters.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './RecurringBlotters.scss';
 import { connect } from 'react-redux';
+import Transaction from '../../model/LineItem';
+import Category from '../../model/Category';
+import { RootState } from '../../redux/store';
+import CategoryComponent from '../../components/category/CategoryComponent';
+
+interface RecurringBlottersProps {
+    transactions: Transaction[];
+    recurringCategories: Category[];
+}
 
 
-class RecurringBlotters extends React.Component<{}, {}> {
+class RecurringBlotters extends React.Component<RecurringBlottersProps, {}> {
 
     constructor(props) {
         super(props);
@@ -18,10 +27,23 @@ class RecurringBlotters extends React.Component<{}, {}> {
     }
 
     render() {
+        let categories = this.props.recurringCategories.map((category, idx) => {
+            let transactions = this.props.transactions.filter(transaction => transaction.category_id === category.id);
+            return <CategoryComponent category={category} key={idx} transactions={transactions} />
+        })
         return (<div className='recurring-blotters-sidebar'>
-
+            <div className='recurring-categories-container'>
+                {categories}
+            </div>
         </div>)
     }
 }
 
-export default RecurringBlotters;
+const mapStateToProps = (state: RootState, ownProps) => {
+    return {
+        transactions: state.transactions.transactions,
+        recurringCategories: state.categories.categories.filter(cat => ['Monthly', 'Yearly'].includes(cat.description))
+    } as RecurringBlottersProps
+}
+
+export default connect(mapStateToProps, {})(RecurringBlotters);
