0e8a131229e700cb484e1a466095eb1c7d3cbe2b
Grant Mulitz <themulitzer@gmail.com>
2021-09-10 23:11:09 -0400

    Allowing all cross origin access

--- server/budgettrackerbackend/budgettrackerbackend/settings.py
+++ server/budgettrackerbackend/budgettrackerbackend/settings.py
@@ -59,9 +59,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000"
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'budgettrackerbackend.urls'
 
